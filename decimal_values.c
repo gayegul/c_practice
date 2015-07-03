fd07deefb2974683127a9afb5a4ab2ae8a6e55e1
Gaye Bulut <gayegul@gmail.com>
2015-07-03 00:18:33 -0700

    add function

---
+++ decimal_values.c
@@ -0,0 +1,16 @@
+/* to display the decimal values
+of x and y on the screen */
+
+#include <stdio.h>
+
+int main(void)
+{
+  int x;
+  unsigned int y;
+
+  x = 0xAB78;
+  y = 0xAB78;
+
+  printf("%i\n", x);
+  printf("%u\n", y);
+}
