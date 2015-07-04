8c9e3ef85e6d29bce211dd4a30be49ae38935d64
Gaye Bulut <gayegul@gmail.com>
2015-07-04 00:01:10 -0700

    add function

---
+++ long_int.c
@@ -0,0 +1,13 @@
+/* to multiply two signed int variables
+with positive values, and display the
+result as a long int */
+
+#include <stdio.h>
+
+int main(void)
+{
+  int x = 13000;
+  int y = 12000;
+
+  printf("%li\n", x * y);
+}
