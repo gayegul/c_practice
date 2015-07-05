6a008f324a9ef7a1fabed0a4e0f7960d70940280
Gaye Bulut <gayegul@gmail.com>
2015-07-05 00:06:29 -0700

    add function

---
+++ negative_to_hex.c
@@ -0,0 +1,11 @@
+/* to display negative integers in hex
+format along with their signed int equivalents */
+
+#include <stdio.h>
+
+int main(void)
+{
+  int x = -23456;
+
+  printf("The hex value if x is 0x%X.\n", x);
+}
